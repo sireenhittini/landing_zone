154e8a1a14e5a6971954627795030649307d6bd3
sireenhittini <SireenE2005@gmail.com>
2025-07-03 13:15:19 +0400

    updated background

--- my-survey/src/App.jsx
+++ my-survey/src/App.jsx
@@ -1,15 +1,10 @@
 
 import './App.css'
 import SurveyForm from './components/SurveyForm';
- 
-export default function App() {
+ export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-xl shadow-lg">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
         <SurveyForm />
-
-      </div>
     </div>
   );
-}
- 
+  }
